7a73ceab6c24baa2bd2de3ca8d4658253390e0ee
Nicolas Kammerdiener <nicolas@kammerdiener.us>
2018-11-12 21:22:13 -0500

    cleanup

--- blockchain-runner.js
+++ blockchain-runner.js
@@ -4,14 +4,14 @@ const { Blockchain, Block } = require('./simpleChain');
 // Initialization
 let blockchain = new Blockchain();
 
-(function theLoop (i) {
-  setTimeout(() => {
-    blockchain.addBlock(new Block(`Test data ${i}`)).then(() => {
-      if (--i) {
-        theLoop(i)
-      }
-    })
-  }, 100);
-})(10);
+setTimeout(() => {
+  for(let i = 0; i < 10; i++) {
+    let newBlock = new Block('Lorem ipsum ' + i);
+    blockchain.addBlock(newBlock)
+      .then((block) => {
+        console.log(block);
+      })
+  }
+}, 10);
 
 setTimeout(() => blockchain.validateChain(), 2000);
